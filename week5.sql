6f76cc54767be3c0283585ffd0234dde936591fb
Md_Echo <allanodiwuor54@gmail.com>
2024-05-02 12:46:14 +0300

    first

---
+++ week5.sql
@@ -0,0 +1,11 @@
+create database StudentsDB;
+use StudentsDB;
+create table students(Student_ID int primary key,
+					First_name text,
+                    last_name text,
+                    Date_of_birth date,
+                    Email text,
+                    Major text);
+select * from students;
+                    
+
